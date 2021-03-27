a419c5e92e2b35af3c070f84a44ce705f32ede1a
Alessandro Solbiati <lessandro@fb.com>
2021-03-27 02:56:43 +0000

    bluetooth pairing 1

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
 var print_message = function(message){
-  document.getElementById('content').appendChild(document.createTextNode(message));
+  var para = document.createElement("p");
+  var node = document.createTextNode(message);
+  para.appendChild(node);
+
+  var element = document.getElementById("content");
+  element.appendChild(para);
 };
 
-console.log('index.js');
-print_message('index.js');
+var bte_setup = function(){
+  var lock = navigator.mozSettings.createLock();
+  var lockCompleteReq = lock.set({
+	'bluetooth.enabled': true
+  });
+
+  result.onsuccess = function() {
+	print_message("bluetooth enabled");
+  }
+
+  result.onerror = function() {
+	print_message("bluetooth not enabled");
+  }
+}
+
+var bte_adapter = function(){
+  console.log('getting adapter..');
+  var adapter;
+  print_message("getting adapter..");
+  var getAdapter = navigator.mozBluetooth.getDefaultAdapter();
+
+  // Retreving the local device adapter is asynchronous, handle this carefully.
+  getAdapter.success = function(evt) {
+	print_message("got adapter 1");
+	print_message(evt);
+	adapter = evt.target.result;
+  }
+  
+  getAdapter.onsuccess = function(evt) {
+	print_message("got adapter 2");
+	print_message(evt);
+	adapter = evt.target.result;
+  }
+
+  getAdapter.onerror = function(evt) {
+	print_message("got adapter 3");
+	print_message(evt);
+	adapter = evt.target.result;
+  }
+}
+
+var main = function(){
+  console.log('index.js');
+  print_message('starting script..');
+  try{
+	bte_setup();
+	bte_adapter();
+  } catch(err){
+	print_message(err)
+  }
+}
+main();
+
 
