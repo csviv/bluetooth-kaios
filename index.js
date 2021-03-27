89c9fd8834f893451997c482eacc2f0502e826e2
Alessandro Solbiati <lessandro@fb.com>
2021-03-27 02:51:06 +0000

    print

---
+++ index.js
@@ -0,0 +1,7 @@
+var print_message = function(message){
+  document.getElementById('content').appendChild(document.createTextNode(message));
+};
+
+console.log('index.js');
+print_message('index.js');
+
